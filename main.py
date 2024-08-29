5615a72ca4d09fe992d969c263f0b3dc88ba6188
Utopia <hanminseok01@gmail.com>
2024-08-29 16:40:58 +0900

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def start():
     print("컴퓨터와의 가위바위보 한 판 승부!")
     print("3번 이기면 나갈 수 있어요")
     print("묵=>1","가위=2", "보=>3")
-    print(rock, scissor, paper)
+    print(rock+scissor+paper)
 
 rock='''
        ,--.--._
@@ -53,44 +53,44 @@ def Game():
         
         
         #1, 2, 3 이외에 다른 키 누름 방지    
-        if str(player) not in ('123'):
+        if player not in (1, 2, 3):
           print("1, 2, 3 중에서만 고르세요!")
           continue
-          
-      #5번 이기면 종료  
-        if win==4:
-          print("축하합니다! 5번 승리하셨습니다")
-          break 
-        else:   
+           
           #묵=1, 가위=2, 보=3
-          if computer==1: #컴퓨터가 묵(1)일 때
-            print(rock)
-            if player==1:
-              draw+=1
-            elif player==2:
-              lose+=1
-            else:
-              win+=1
+        if computer==1: #컴퓨터가 묵(1)일 때
+          print(rock)
+          if player==1:
+            draw+=1
+          elif player==2:
+            lose+=1
+          else:
+             win+=1
               
-          elif computer==2: #컴퓨터가 가위(2)일 때
-            print(scissor)
-            if player==1:
-              win+=1
-            elif player==2:
-              draw+=1
-            else:
-              lose+=1
-              
-          elif computer==3: #컴퓨터가 보(3)일 때
-            print(paper)
-            if player==1:
-              lose+=1
-            elif player==2:
-              win+=1
-            else:
-              draw+=1
-              
-          
+        elif computer==2: #컴퓨터가 가위(2)일 때
+           print(scissor)
+           if player==1:
+             win+=1
+           elif player==2:
+             draw+=1
+           else:
+             lose+=1
+             
+        elif computer==3: #컴퓨터가 보(3)일 때
+          print(paper)
+          if player==1:
+            lose+=1
+          elif player==2:
+            win+=1
+          else:
+            draw+=1
+            
+        #5번 이기면 종료
+        if win==5:
+          print("축하합니다! 5번 승리하셨습니다")
+          break
+     
+    input("게임이 종료되었습니다. Enter키를 눌러 창을 닫아주세요")     
           
       
         
